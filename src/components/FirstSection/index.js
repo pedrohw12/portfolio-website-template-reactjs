f1689d539942cc0270a23be1e1bc1e6429506c9b
Pedro Wanderley <peu-zico@hotmail.com>
2021-02-04 00:14:00 -0300

    solving problem

--- src/components/FirstSection/index.js
+++ src/components/FirstSection/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+// import { client } from "../../config/prismic";
+// import Prismic from "prismic-javascript";
 
 // Components
 import Item from "./components/Item";
@@ -7,6 +9,7 @@ import Item from "./components/Item";
 import { Container, Content, Title, Explanation, CardsArea } from "./styles";
 
 const FirstSection = () => {
+  // const [content, setContent] = useState([]);
   const itens = [
     {
       title: "Atraia",
@@ -15,8 +18,7 @@ const FirstSection = () => {
     },
     {
       title: "Encante",
-      description:
-        "A melhor propaganda é feita por um cliente satisfeito.",
+      description: "A melhor propaganda é feita por um cliente satisfeito.",
     },
     {
       title: "Venda",
@@ -24,14 +26,32 @@ const FirstSection = () => {
     },
   ];
 
+  // async function fetchApiContent() {
+  //   const response = await client().query(
+  //     Prismic.Predicates.at("document.type", "header_description")
+  //   );
+
+  //   if (response) {
+  //     console.log("RESPONSE", response.results);
+  //     setContent(response.results);
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   fetchApiContent();
+  // }, []);
+
   return (
     <Container>
       <Content className="container-wrap">
+        {/* <Title>{content[0]?.data.header_title[0].text}</Title>
+        <Explanation>{content[0]?.data.description[0].text}</Explanation> */}
         <Title>Quer mais clientes?</Title>
         <Explanation>
           Eu posso te ajudar fazendo a internet trabalhar 24 horas para o seu
           negócio. Parece mágica! Mas é apenas utilizar a melhor ferramenta do
-          mundo, para impactar pessoas e fazê-las entender o valor do seu negócio.
+          mundo, para impactar pessoas e fazê-las entender o valor do seu
+          negócio.
         </Explanation>
         <CardsArea>
           {itens.map((item, key) => (
